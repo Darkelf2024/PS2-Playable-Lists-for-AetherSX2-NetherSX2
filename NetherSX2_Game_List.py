e122cd077955a5e342e30204a6dc9c01683fa781
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2026-01-20 01:32:49 -0600

    Add files via upload

---
+++ NetherSX2_Game_List.py
@@ -0,0 +1,65 @@
+# NetherSX2 Game List 
+
+**Notes**
+- Includes all entries from the original NetherSX2 list plus PS2 Tweaked Games.
+- List is fully alphabetized by game title.
+- Please Read [Legal Notice & Disclaimer](https://github.com/Darkelf2024/PS2-Playable-Lists-for-AetherSX2-NetherSX2/blob/main/Legal_Notice_and_Disclaimer.md)
+Please Read [Darkelf Definitions List](https://github.com/Darkelf2024/Darkelf-Retro-Lounge/blob/main/Darkelf%20Lounge%20Definitions%20List.md)
+
+| Game | EE Cycle Rate | EE Skip | Affinity Control | Vsync/Threads | Blending | Graphics | Hardware Download | Region | Audio | Tested |
+|:-----|:--------------|:--------|:-----------------|--------------:|:---------|:---------|:------------------|:-------|:------|:-------|
+| ACE Combat: Distant Thunder | -3 | -1 | Performance | 3 | Medium | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| ACE Combat: Squandron Leader | -3 | -3 | Disabled | 3 | Medium | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| ATV Off Road: Off Road Fury | -3 | -1 | Disabled | 3 | Ultra | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| Black | -3 | -2 | Disabled | 3 | Full | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| Bloody Roar 4 | -3 | Default | Disabled | 3 | Ultra | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| Bully | -3 | -1 | Performance | 3 | Ultra | OpenGL | Unsynchronized | US | Async Mix | NetherSX2 |
+| Burnout 3: Takedown | -3 | -1 | Disabled | 3 | Ultra | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| Burnout Dominator | -3 | -3 | Disabled | 3 | Ultra | Vulkan | Disabled Readbacks | PAL | Async Mix | NetherSX2 |
+| Call of Duty: Finest Hour | -2 | -2 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Castlevania: Lament of Innocence | -2 | Default | Performance | 3 | — | Vulkan | — | US | Async Mix | AetherSX2 |
+| Chaos Legion | -3 | -1 | Disabled | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Devil May Cry | -3 | Default | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| FIFA Street 2 | -3 | -3 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Fight Night Round 2 | -3 | -2 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Fight Night Round 3 | -3 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Final Fantasy X-2 | -3 | -2 | Performance | 3 | Ultra | Software | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Gauntlet Dark Legacy | -2 | -2 | Performance | 3 | Ultra | OpenGL | Accurate | PAL | Async Mix | NetherSX2 |
+| Ghost Rider | -3 | -3 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| God Hand | -3 | -2 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| God of War | -3 | -3 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Godzilla: Save the Earth | -3 | Default | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Jak and Daxter | -3 | -1 | Disabled | 3 | Ultra | OpenGL | Accurate | PAL | Async Mix | NetherSX2 |
+| Jet Li: Rise to Honor | -3 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Justice League Heroes | -3 | -2 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Klonoa 2 | -1 | -1 | Disabled | 3 | Default | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| LEGO Star Wars II: The Original Trilogy | -3 | -2 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Manhunt | -2 | Default | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Marvel: Ultimate Alliance | -2 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Medal of Honor: Frontline | -3 | Default | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Micro Machines v4 | -3 | -1 | Disabled | 3 | Ultra | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| Midnight Club 2 | -1 | Default | Disabled | 3 | Ultra | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| Mortal Kombat: Armageddon | -3 | -3 | Disabled | 3 | Ultra | OpenGL | Accurate | PAL | Async Mix | NetherSX2 |
+| Mortal Kombat: Deception | -3 | -3 | Disabled | 3 | Ultra | Vulkan | Disabled | PAL | Async Mix | NetherSX2 |
+| Mortal Kombat: Shaolin Monks | -3 | -3 | Disabled | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| NBA Street Vol. 3 | -1 | -1 | Disabled | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Need for Speed: Hot Pursuit 2 | -3 | -1 | Disabled | 3 | Ultra | Vulkan | Disabled Readbacks | PAL | Async Mix | NetherSX2 |
+| Need for Speed: Underground | -3 | -2 | Performance | 3 | Ultra | OpenGL | Unsynchronized | PAL | L/TS | NetherSX2 |
+| Okami | -3 | Default | Disabled | 3 | Ultra | Vulkan | Accurate | PAL | Async Mix | NetherSX2 |
+| OutRun 2006 | -3 | -3 | Performance | 3 | — | Software | — | PAL | Async Mix | AetherSX2 |
+| PES 2011 | -3 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Pro Evolution Soccer 2008 | -3 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Red Faction | -3 | -1 | Disabled | 3 | Ultra | Vulkan | Disabled Readbacks | PAL | Async Mix | NetherSX2 |
+| Red Faction II | -3 | -2 | Disabled | 3 | Ultra | OpenGL | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Rumble Racing | 0 | 0 | Disabled | 3 | Ultra | Vulkan | Disabled | PAL | Async Mix | NetherSX2 |
+| Shox | -3 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Sly Cooper | -3 | -3 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Spider-Man 3 | -3 | Default | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| SSX On Tour | -1 | -1 | Disabled | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Star Wars Battlefront II | -3 | -3 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Thrillville: Off the Rails | -1 | -1 | Disabled | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Timesplitters: Black | Default | Default | Performance | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Twisted Metal: Black | Default | Default | Performance | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Ty 2: Bush Rescue | -2 | -1 | Performance | 3 | — | Vulkan | — | PAL | Async Mix | AetherSX2 |
+| Urban Reign | -1 | -1 | Disabled | 3 | Ultra | Vulkan | Unsynchronized | PAL | Async Mix | NetherSX2 |
+| Valkyrie Profile 2 | -3 | -3 | Performance | 3 | Ultra | OpenGL | Unsynchronized | PAL | Async Mix | NetherSX2 |
